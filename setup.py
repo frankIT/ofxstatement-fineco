4fded03c558c2c0d79563f3f8fbcf8be09b61278
Andrey Lebedev <andrey@lebedev.lt>
2020-09-14 08:35:15 +0300

    Blacken source code and add type annotations

--- setup.py
+++ setup.py
@@ -6,35 +6,36 @@ from distutils.core import setup
 
 version = "0.0.1"
 
-with open('README.rst') as f:
+with open("README.rst") as f:
     long_description = f.read()
 
-setup(name='ofxstatement-sample',
-      version=version,
-      author="Andrey Lebedev",
-      author_email="andrey@lebedev.lt",
-      url="https://github.com/kedder/ofxstatement",
-      description=("Sample plugin for ofxstatement"),
-      long_description=long_description,
-      license="GPLv3",
-      keywords=["ofx", "banking", "statement"],
-      classifiers=[
-          'Development Status :: 3 - Alpha',
-          'Programming Language :: Python :: 3',
-          'Natural Language :: English',
-          'Topic :: Office/Business :: Financial :: Accounting',
-          'Topic :: Utilities',
-          'Environment :: Console',
-          'Operating System :: OS Independent',
-          'License :: OSI Approved :: GNU General Public License v3 (GPLv3)'],
-      packages=find_packages('src'),
-      package_dir={'': 'src'},
-      namespace_packages=["ofxstatement", "ofxstatement.plugins"],
-      entry_points={
-          'ofxstatement':
-          ['sample = ofxstatement.plugins.sample:SamplePlugin']
-          },
-      install_requires=['ofxstatement'],
-      include_package_data=True,
-      zip_safe=True
-      )
+setup(
+    name="ofxstatement-sample",
+    version=version,
+    author="Andrey Lebedev",
+    author_email="andrey@lebedev.lt",
+    url="https://github.com/kedder/ofxstatement",
+    description=("Sample plugin for ofxstatement"),
+    long_description=long_description,
+    license="GPLv3",
+    keywords=["ofx", "banking", "statement"],
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Programming Language :: Python :: 3",
+        "Natural Language :: English",
+        "Topic :: Office/Business :: Financial :: Accounting",
+        "Topic :: Utilities",
+        "Environment :: Console",
+        "Operating System :: OS Independent",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+    ],
+    packages=find_packages("src"),
+    package_dir={"": "src"},
+    namespace_packages=["ofxstatement", "ofxstatement.plugins"],
+    entry_points={
+        "ofxstatement": ["sample = ofxstatement.plugins.sample:SamplePlugin"]
+    },
+    install_requires=["ofxstatement"],
+    include_package_data=True,
+    zip_safe=True,
+)
--- src/ofxstatement/plugins/sample.py
+++ src/ofxstatement/plugins/sample.py
@@ -1,36 +1,35 @@
+from typing import Iterable
+
 from ofxstatement.plugin import Plugin
 from ofxstatement.parser import StatementParser
-from ofxstatement.statement import StatementLine
+from ofxstatement.statement import Statement, StatementLine
 
 
 class SamplePlugin(Plugin):
-    """Sample plugin (for developers only)
-    """
+    """Sample plugin (for developers only)"""
 
-    def get_parser(self, filename):
+    def get_parser(self, filename: str) -> "SampleParser":
         return SampleParser(filename)
 
 
-class SampleParser(StatementParser):
-    def __init__(self, filename):
+class SampleParser(StatementParser[str]):
+    def __init__(self, filename: str) -> None:
+        super().__init__()
         self.filename = filename
 
-    def parse(self):
+    def parse(self) -> Statement:
         """Main entry point for parsers
 
         super() implementation will call to split_records and parse_record to
         process the file.
         """
         with open(self.filename, "r") as f:
-            self.input = f
-            return super(SampleParser, self).parse()
+            return super().parse()
 
-    def split_records(self):
-        """Return iterable object consisting of a line per transaction
-        """
+    def split_records(self) -> Iterable[str]:
+        """Return iterable object consisting of a line per transaction"""
         return []
 
-    def parse_record(self, line):
-        """Parse given transaction line and return StatementLine object
-        """
+    def parse_record(self, line: str) -> StatementLine:
+        """Parse given transaction line and return StatementLine object"""
         return StatementLine()
