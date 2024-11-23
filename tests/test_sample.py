f786da56ab501eec580eb4deb23702240b98fd03
FrankIT <fracchiaman@gmail.com>
2024-11-23 21:28:31 +0100

    Provided 2 sample statement files and implemented the test over them.

--- tests/test_sample.py
+++ tests/test_sample.py
@@ -1,16 +1,23 @@
 import os
 
 from ofxstatement.ui import UI
-
 from ofxstatement_fineco.plugin import FinecoPlugin
-
 
 def test_sample() -> None:
     plugin = FinecoPlugin(UI(), {})
     here = os.path.dirname(__file__)
-    sample_filename = os.path.join(here, "sample-statement.csv")
+    
+    # NOTE: The two sample files provided has been edidted with libreoffice
+    # and although they keep the same layout and spreadsheet format as the original ones,
+    # they do slightly differ in their raw content (metadata, cell types ecc).
 
-    parser = plugin.get_parser(sample_filename)
-    statement = parser.parse()
-
-    assert statement is not None
+    test_files = [
+        os.path.join(here, "movements.xlsx"), # Savings statement
+        os.path.join(here, "my-cards.xls") # Cards statement
+    ]
+    
+    for filename in test_files:
+        parser = plugin.get_parser(filename)
+        statement = parser.parse()
+        
+        assert statement is not None
